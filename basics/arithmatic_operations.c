5f32ba2495b5134406df2da537578535f438ec58
janhavisagdeo <janhavisagdeo7@gmail.com>
2026-02-01 18:38:05 +0530

    Create arithmatic_operations.c

---
+++ basics/arithmatic_operations.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  float a,b;
+    printf("enter the numbers: ");
+    scanf("%f%f", &a,&b);
+    printf("result 1 = %f\n", a+b);
+    printf("result 2 = %f\n", a-b);
+    printf("result 3 = %f\n", a*b);
+    printf("result 4 = %f\n", a/b);
+    
+}
