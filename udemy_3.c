5428d578ec64d2c2378aff3faf94853399580ab8
janhavisagdeo <janhavisagdeo7@gmail.com>
2026-03-29 15:09:34 +0530

    Create udemy_3.c

---
+++ udemy_3.c
@@ -0,0 +1,17 @@
+ARITHMETIC OPERATIONS
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    int z, m = 3, n = 2;
+    double x, y, p = 2.0;
+
+    x = m / p;
+    y = m / n;
+    z = m / p;
+
+    printf("X : %f\nY: %f\nZ: %d\n\n", x, y, z);
+
+    return 0;
+}
