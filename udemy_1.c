4c013d269a6925af02f814101106415cdc92b5d1
janhavisagdeo <janhavisagdeo7@gmail.com>
2026-03-29 15:08:09 +0530

    Create udemy_1.c

---
+++ udemy_1.c
@@ -0,0 +1,15 @@
+RETURN FUNCTION 
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    int first, second, third;
+
+    printf("Please enter the values for first, second and three by letting a space between them:");
+    scanf("%d %d %d", &first, &second, &third);
+
+    printf("The values you have entered are\nfirst : %d\nsecond : %d\nthird : %d\n", first, second, third);
+
+    return (111);
+}
