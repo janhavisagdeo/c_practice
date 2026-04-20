7a7589ab792a75bb44ef8129d675e429a29e10a2
janhavisagdeo <janhavisagdeo7@gmail.com>
2026-04-20 11:29:54 +0530

    Create sine_series.c

---
+++ sine_series.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float x, term, sum = 0;
+    int sign = 1;
+
+    printf("Enter value of x (in radians): ");
+    scanf("%f", &x);
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    term = x;  // first term
+
+    for (i = 1; i <= n; i++) {
+        sum += sign * term;
+
+        // next term calculation
+        term = term * x * x / ((2*i) * (2*i + 1));
+        sign = -sign;
+    }
+
+    printf("Sum of sine series = %f", sum);
+
+    return 0;
+}
